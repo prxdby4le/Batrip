ff982bcca6141ff5428d800cea9de6b9da828bf0
prxdby4le <kleber0611@gmail.com>
2025-08-31 21:01:09 -0300

    d

--- database/batrip_alter_users.sql
+++
@@ -1,6 +0,0 @@
--- Adiciona campos de endereço, cidade, estado e cep na tabela users
-ALTER TABLE users
-ADD COLUMN endereco VARCHAR(255) DEFAULT NULL,
-ADD COLUMN cidade VARCHAR(100) DEFAULT NULL,
-ADD COLUMN estado VARCHAR(2) DEFAULT NULL,
-ADD COLUMN cep VARCHAR(10) DEFAULT NULL;
--- database/batrip_full.sql
+++ database/batrip_full.sql
@@ -68,5 +68,3 @@ VALUES (
 ON DUPLICATE KEY UPDATE 
   is_admin=1,
   password=VALUES(password);
-
-UPDATE users SET password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi' WHERE email = 'admin@batrip.com';
